117cc1fc25ebba7c1ab9a05c677c120d93304453
EC2 Default User <ec2-user@ip-172-31-3-238.us-east-2.compute.internal>
2021-08-20 20:10:14 +0000

    modify image paths

--- data.js
+++ data.js
@@ -17,7 +17,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe",
      link: "https://www.imdb.com/title/tt0092067/",
-    image: "./_images/CastleInTheSky.gif"  
+    image: "./images/CastleInTheSky.gif"
   },
   {
     id: "12cfb892-aac0-4c5b-94af-521852e46d6a",
@@ -37,7 +37,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/12cfb892-aac0-4c5b-94af-521852e46d6a",
         link: "https://www.imdb.com/title/tt0095327/?ref_=nv_sr_srsg_0",
-    image: "./_images/GraveFireflies.gif"  
+    image: "./images/GraveFireflies.gif"  
   },
   {
     id: "58611129-2dbc-4a81-a72f-77ddfc1b1b49",
@@ -70,7 +70,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49",
         link: "https://www.imdb.com/title/tt0096283/?ref_=nv_sr_srsg_0",
-    image: "./_images/Totoro.gif"
+    image: "./images/Totoro.gif"
   },
   {
     id: "ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
@@ -90,7 +90,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
     link: "https://www.imdb.com/title/tt0097814/?ref_=nv_sr_srsg_0",
-    image: "./_images/Kiki.gif"
+    image: "./images/Kiki.gif"
   },
   {
     id: "4e236f34-b981-41c3-8c65-f8c9000b94e7",
@@ -110,7 +110,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/4e236f34-b981-41c3-8c65-f8c9000b94e7",
         link: "https://www.imdb.com/title/tt0102587/?ref_=nv_sr_srsg_0",
-    image: "./_images/Yesterday.gif"
+    image: "./images/Yesterday.gif"
   },
   {
     id: "ebbb6b7c-945c-41ee-a792-de0e43191bd8",
@@ -130,7 +130,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/ebbb6b7c-945c-41ee-a792-de0e43191bd8",
         link: "https://www.imdb.com/title/tt0104652/?ref_=nv_sr_srsg_0",
-    image: "./_images/Porco.gif"  
+    image: "./images/Porco.gif"  
   },
   {
     id: "1b67aa9a-2e4a-45af-ac98-64d6ad15b16c",
@@ -150,7 +150,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/1b67aa9a-2e4a-45af-ac98-64d6ad15b16c",
      link: "https://www.imdb.com/title/tt0110008/?ref_=nv_sr_srsg_0",
-    image: "./_images/PomPoko.gif"  
+    image: "./images/PomPoko.gif"  
   },
   {
     id: "ff24da26-a969-4f0e-ba1e-a122ead6c6e3",
@@ -170,7 +170,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/ff24da26-a969-4f0e-ba1e-a122ead6c6e3",
        link: "https://www.imdb.com/title/tt0113824/?ref_=nv_sr_srsg_0",
-    image: "./_images/Whisper.gif"  
+    image: "./images/Whisper.gif"  
   },
   {
     id: "0440483e-ca0e-4120-8c50-4c8cd9b965d6",
@@ -203,7 +203,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6",
         link: "https://www.imdb.com/title/tt0119698/?ref_=nv_sr_srsg_0",
-    image: "./_images/Mononoke.gif"  
+    image: "./images/Mononoke.gif"  
   },
   {
     id: "45204234-adfd-45cb-a505-a8e7a676b114",
@@ -223,7 +223,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/45204234-adfd-45cb-a505-a8e7a676b114",
         link: "https://www.imdb.com/title/tt0206013/?ref_=nv_sr_srsg_0",
-    image: "./_images/Yamadas.gif"  
+    image: "./images/Yamadas.gif"  
   },
   {
     id: "dc2e6bd1-8156-4886-adff-b39e6043af0c",
@@ -243,7 +243,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/dc2e6bd1-8156-4886-adff-b39e6043af0c",
       link: "https://www.imdb.com/title/tt0245429/?ref_=nv_sr_srsg_0",
-    image: "./_images/Spirited.gif"  
+    image: "./images/Spirited.gif"  
   },
   {
     id: "90b72513-afd4-4570-84de-a56c312fdf81",
@@ -263,7 +263,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/90b72513-afd4-4570-84de-a56c312fdf81",
       link: "https://www.imdb.com/title/tt0347618/?ref_=nv_sr_srsg_0",
-    image: "./_images/TheCat.gif"  
+    image: "./images/TheCat.gif"  
   },
   {
     id: "cd3d059c-09f4-4ff3-8d63-bc765a5184fa",
@@ -283,7 +283,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa",
      link: "https://www.imdb.com/title/tt0347149/?ref_=nv_sr_srsg_0",
-    image: "./_images/Howls.gif"  
+    image: "./images/Howls.gif"  
   },
   {
     id: "112c1e67-726f-40b1-ac17-6974127bb9b9",
@@ -303,7 +303,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/112c1e67-726f-40b1-ac17-6974127bb9b9",
       link: "https://www.imdb.com/title/tt0495596/?ref_=nv_sr_srsg_0",
-    image: "./_images/Earthsea.gif"  
+    image: "./images/Earthsea.gif"  
   },
   {
     id: "758bf02e-3122-46e0-884e-67cf83df1786",
@@ -323,7 +323,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/758bf02e-3122-46e0-884e-67cf83df1786",
      link: "https://www.imdb.com/title/tt0876563/?ref_=nv_sr_srsg_0",
-    image: "./_images/Ponyo.gif"  
+    image: "./images/Ponyo.gif"  
   },
   {
     id: "2de9426b-914a-4a06-a3a0-5e6d9d3886f6",
@@ -343,7 +343,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/2de9426b-914a-4a06-a3a0-5e6d9d3886f6",
         link: "https://www.imdb.com/title/tt1568921/?ref_=nv_sr_srsg_0",
-    image: "./_images/Arrietty.gif"  
+    image: "./images/Arrietty.gif"  
   },
   {
     id: "45db04e4-304a-4933-9823-33f389e8d74d",
@@ -363,7 +363,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/45db04e4-304a-4933-9823-33f389e8d74d",
         link: "https://www.imdb.com/title/tt1798188/?ref_=nv_sr_srsg_0",
-    image: "./_images/Poppy.gif"  
+    image: "./images/Poppy.gif"  
   },
   {
     id: "67405111-37a5-438f-81cc-4666af60c800",
@@ -383,7 +383,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/67405111-37a5-438f-81cc-4666af60c800",
        link: "https://www.imdb.com/title/tt2013293/?ref_=nv_sr_srsg_0",
-    image: "./_images/Wind.gif"  
+    image: "./images/Wind.gif"  
   },
   {
     id: "578ae244-7750-4d9f-867b-f3cd3d6fecf4",
@@ -403,7 +403,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/578ae244-7750-4d9f-867b-f3cd3d6fecf4",
         link: "https://www.imdb.com/title/tt2576852/?ref_=nv_sr_srsg_2",
-    image: "./_images/Kaguya.gif"  
+    image: "./images/Kaguya.gif"  
   },
   {
     id: "5fdfb320-2a02-49a7-94ff-5ca418cae602",
@@ -423,7 +423,7 @@ var data = [
     url:
       "https://ghibliapi.herokuapp.com/films/5fdfb320-2a02-49a7-94ff-5ca418cae602",
         link: "https://www.imdb.com/title/tt3398268/?ref_=nv_sr_srsg_0",
-    image: "./_images/Marnie.gif"  
+    image: "./images/Marnie.gif"  
   },
 ];
 
